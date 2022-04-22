0d481a5debf08224cd9707b65fc2caf483d070f2
Stef Altavista <stefanomsc@hotmail.it>
2022-04-22 16:40:26 +0200

    push database only, the rest is work in progress

--- database/db.js
+++ database/db.js
@@ -1,15 +1,45 @@
 const spicedPg = require("spiced-pg");
 
 const db = spicedPg("postgres:postgres:postgres@localhost:5432/petition");
+var where;
 
-module.exports.query = () => db.query("SELECT * FROM signatures;");
-module.exports.queryId = (id) =>
-    db.query("SELECT * FROM signatures WHERE id = $1", [id]);
-module.exports.add = (name, surname, age, city, country, signature) => {
+module.exports.query = (table) => db.query(`SELECT * FROM ${table};`);
+
+module.exports.signatoires = (ct, cn) => {
+    if (ct) {
+        where = ` WHERE LOWER(userData.city) = LOWER('${ct}')`;
+    } else if (cn) {
+        where = ` WHERE LOWER(userData.country) = LOWER('${cn}')`;
+    } else {
+        where = "";
+    }
+
+    return db.query(`SELECT userData.name, userData.age, userData.city, userData.country, userData.link 
+                                                FROM users
+                                                JOIN userData
+                                                ON users.id = userData.user_id
+                                                ${where};`);
+};
+
+module.exports.queryBy = (table, x, val) =>
+    db.querydb.query(`SELECT * FROM ${table} WHERE ${x} = ${val}`);
+module.exports.register = (email, hash) => {
     return db.query(
-        `INSERT INTO signatures (name, surname, age, city, country, signature) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
-        [name, surname, age, city, country, signature]
+        `INSERT INTO users (email,hash) VALUES( $1 , $2) RETURNING id`,
+        [email, hash]
     );
 };
 
-module.exports.count = () => db.query("SELECT COUNT(id) FROM signatures");
+module.exports.add = (name, surname, age, city, country) => {
+    return db.query(
+        `INSERT INTO userData (name, surname, age, city, country) VALUES ($1, $2, $3, $4, $5) RETURNING id`,
+        [name, surname, age, city, country]
+    );
+};
+
+module.exports.sign = (signature) => {
+    return db.query(
+        `INSERT INTO signature (signature) VALUES ($1) RETURNING id`,
+        [signature]
+    );
+};
--- database/petition.sql
+++ database/petition.sql
@@ -1,12 +1,56 @@
-DROP TABLE IF EXISTS signatures;
 
-CREATE TABLE signatures (
-     id SERIAL PRIMARY KEY,
-     name VARCHAR NOT NULL CHECK (name != ''),
-     surname VARCHAR NOT NULL CHECK (surname != ''),
-     age int NOT NULL,
-     city VARCHAR NOT NULL CHECK (name != ''),
-     country VARCHAR NOT NULL CHECK (country !=''),
-     signature VARCHAR NOT NULL CHECK (signature !='')
-    
+DROP TABLE IF EXISTS userData;
+DROP TABLE IF EXISTS signatures;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+     id             SERIAL PRIMARY KEY,
+     email          VARCHAR UNIQUE NOT NULL CHECK (email != ''),
+     hash           VARCHAR NOT NULL CHECK (hash != ''),
+     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+     
 );
+CREATE TABLE userData(
+     id             SERIAL PRIMARY KEY,
+     user_id        INT NOT NULL UNIQUE REFERENCES users(id), 
+     name           VARCHAR,  
+     surname        VARCHAR,  
+     age            INT, 
+     city           VARCHAR, 
+     country        VARCHAR, 
+     link           VARCHAR
+);
+CREATE TABLE signatures(
+     id             SERIAL PRIMARY KEY,
+     user_id        INT NOT NULL UNIQUE REFERENCES users(id),
+     signature      VARCHAR NOT NULL CHECK (signature !='')
+
+);
+
+--DUMMY DATA--
+
+
+INSERT INTO users(email, hash) 
+VALUES ( 'stefano@altavista.com' , 'mypasswordhashedinacrazyway');
+INSERT INTO users(email, hash) 
+VALUES ( 'stefano@spices.com' , 'anotherpasswordhashedinacrazyway');
+INSERT INTO users(email, hash) 
+VALUES ( 'jimmy@hendrix.com' , 'jimmyspasswordhashedinacrazyway');
+INSERT INTO users(email, hash) 
+VALUES ( 'alexander@thegreat' , 'alexanderspasswordhashedinacrazyway');
+
+INSERT INTO userData(user_id, name, surname, age,city,country,link) 
+VALUES (1, 'Stefano', 'Altavista', 30, 'Berlin', 'Germany', 'google.com');
+INSERT INTO userData(user_id, name, surname, age,city,country,link) 
+VALUES (2, 'Spicy', 'Stef', 30, 'Berlin', 'Germany', 'google.com');
+INSERT INTO userData(user_id, name, surname, age,city,country,link) 
+VALUES (3, 'Jimmy', 'Hendrix', 27, 'Woodstock', 'USA', 'google.com');
+INSERT INTO userData(user_id, name, surname, age,city,country,link) 
+VALUES (4, 'Alexander', 'Thegreat', 30, 'Athens', 'Greece', 'google.com');
+
+INSERT INTO signatures(user_id,signature) 
+VALUES (1,'mysigaturecodifiedinacrazyway');
+INSERT INTO signatures(user_id,signature) 
+VALUES (3,'jimmyssigaturecodifiedinacrazyway');
+INSERT INTO signatures(user_id,signature) 
+VALUES (4,'alexanderssigaturecodifiedinacrazyway');
